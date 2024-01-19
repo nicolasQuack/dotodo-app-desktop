bfeac36aa16278eaacf44497d40c4f7a688a2349
nicolasQuack <nicolas.quack@icloud.com>
2024-01-19 00:47:22 -0300

    chore(tailwind.config.ts): configuring tailwind config;

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,32 @@
+import flowbite from 'flowbite/plugin'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
+    './node_modules/flowbite-react/lib/**/*.js',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      'background-dark': '#1B2845'
-    }
+    extend: {
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+        },
+      },
+    },
   },
-  plugins: [],
-}
-export default config
+  plugins: [flowbite],
+};
+
+export default config;
