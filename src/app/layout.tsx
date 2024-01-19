6f3cbff279436c4b06c8e080808cc63be438c688
nicolasQuack <nicolas.quack@icloud.com>
2024-01-19 04:00:40 -0300

    chore(app/): changing metadata;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
-    default: 'DoToDo',
+    default: 'Tarefas',
     template: '%s | DoToDo'
   },
   description: 'Do to do right!',
