45b9dfeb664e60e9bd7d8f2a1663c7212e172992
nicolasQuack <nicolas.quack@icloud.com>
2024-01-19 06:04:47 -0300

    style(utils/): importing Inter font from Next/font/google;

--- src/app/utils/fonts.ts
+++ src/app/utils/fonts.ts
@@ -1,6 +1,8 @@
-import { Pacifico } from "next/font/google";
+import { Pacifico, Inter } from "next/font/google";
 
 export const pacifico = Pacifico({
     weight: '400',
     subsets: ['latin'],
 })
+
+export const inter = Inter({subsets: ['latin']})
