7ea80d900d320ae8837261322ed74927fff7011a
nicolasQuack <nicolas.quack@icloud.com>
2024-01-23 15:51:05 -0300

    style(/(dashboard)/ui): adding the collapsed context to delay re-rendering in content;

--- src/app/(dashboard)/ui/content.tsx
+++ src/app/(dashboard)/ui/content.tsx
@@ -5,22 +5,28 @@ import Image from "next/image"
 import type { FC } from "react";
 import React, { useState } from "react";
 import { AlertExample } from "./alert";
-import { InputExample } from "./input"
+import { InputExample } from "./input";
 import { TableExample } from "./table";
 import { CardExample } from "./cards";
+import { useSidebarContext } from "../../../../context/sidebar-context";
+import { twMerge } from "tailwind-merge";
+
 
 export const HomePageContent: NextPage = function () {
+
+    const { isCollapsed } = useSidebarContext();
+
     return (
-        <div className="p-6 h-screen">
+        <div className={twMerge("p-6 h-screen", isCollapsed ? "aria-disabled: true" : "aria-disabled:true")}>
             <section className="pb-6">
                 <AlertExample />
             </section>
             <section>
-                <div className="pb-6">
+                <div className="pb-6 ">
                     <InputExample />
                     <Image
                         loading="lazy"
-                        className="pt-5 ml-16 dark:opacity-50 md:block hidden"
+                        className={twMerge("pt-5 ml-5 dark:opacity-50 md:block hidden transition-colors duration-1000", isCollapsed ? "hidden" : "")}
                         alt="Dotodo template"
                         height="126"
                         src="/img/template.png"
