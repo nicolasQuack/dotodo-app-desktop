41360d47303b70118a6d8b3932c2f0fb26f1fd0d
nicolasQuack <nicolas.quack@icloud.com>
2024-01-27 00:21:56 -0300

    feat(context/): create and recover tasks;

--- context/task-context.tsx
+++ context/task-context.tsx
@@ -1,19 +1,36 @@
 "use client";
 
-import { FC, createContext, PropsWithChildren, useState, useContext } from "react";
+import { FC, createContext, PropsWithChildren, useState, useContext, useEffect } from "react";
 import { Task } from "../@types";
 
 interface TodosProviderProps {
     tasks: Task[];
+    createTask: (description: string) => void;
 }
 
 const TodosContext = createContext<TodosProviderProps>({} as TodosProviderProps);
 
+const TASKS_KEY = "@dotodo/tasks"
+
 export const TodosProvider: FC<PropsWithChildren> = function ({ children }) {
     const [tasks, setTasks] = useState<Task[]>([])
 
+    useEffect(() => {
+        const tasks = localStorage.getItem(TASKS_KEY) || "[]";
+        const formatedTasks = JSON.parse(tasks);
+
+        setTasks(formatedTasks);
+    }, [])
+
+    function createTask(description: string) {
+        const task: Task = { description, createdAt: new Date(), isCompleted: false };
+        const newTasks = [...tasks, task];
+        setTasks(newTasks);
+        localStorage.setItem(TASKS_KEY, JSON.stringify(newTasks));
+    }
+
     return (
-        <TodosContext.Provider value={{ tasks }}>
+        <TodosContext.Provider value={{ tasks, createTask }}>
             {children}
         </TodosContext.Provider>
     )
